b78016e47b2a8d2ed24d64069206bd12fade301d
yangnibal <wonjun9670@naver.com>
2020-06-08 15:14:24 +0900

    design

--- src/pages/Inf.js
+++ src/pages/Inf.js
@@ -9,9 +9,9 @@ class Inf extends React.Component{
             <div className="grademanage-container">
                 <Header/>
                 <div className="grademanage-content-container">
-                    <Link className="grademanage-content" to="/inf/vid">동영상 자료</Link>
-                    <Link className="grademanage-content" to="/inf/mat">인터넷 검색 자료</Link>
-                    <Link className="grademanage-content" to="/">파일 출력 가능 자료</Link>
+                    <Link className="inf-content" to="/inf/vid">동영상 자료</Link>
+                    <Link className="inf-content" to="/inf/mat">인터넷 검색 자료</Link>
+                    <Link className="inf-content" to="/">파일 출력 가능 자료</Link>
                 </div>
             </div>
         )
