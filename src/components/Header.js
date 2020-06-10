d32f8303fbb532397f04263da1db36347347a22c
yangnibal <wonjun9670@naver.com>
2020-06-10 21:59:05 +0900

    no message

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
                 <Link to="/ac" className="header-data-content">성적 등급</Link>
                 <Link to="/" className="header-data-content">교재 출력</Link>
             </div>
-            <div className="link-dae-home">대외비 블로그<br/>바로가기</div>
+            <a href="https://blog.naver.com/dreamlearnforu" className="link-dae-home">대외비 블로그<br/>바로가기</a>
             <a href="http://dreamrun.itforone.co.kr/bbs/login.php" className="link-dae-app">[대외비 APP 학습 관리]<br/>프로그램 바로가기</a>
             {token===null? <Link className="link-login" to="/account/login">로그인</Link> :
             <span className="span"><Link className="link-login" to="/account/login">로그아웃</Link>&nbsp;/&nbsp;<Link className="link-login" to="/account/edit">내정보 수정</Link></span> }
