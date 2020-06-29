3208478b1275f29d53ea455c6229966f89f5ab30
yangnibal <wonjun9670@naver.com>
2020-06-29 18:40:48 +0900

    no message

--- src/pages/Inf.js
+++ src/pages/Inf.js
@@ -39,7 +39,7 @@ class Inf extends React.Component{
                 <div className="grademanage-content-container">
                     <Link className="inf-content" to="/inf/vid">동영상 자료</Link>
                     <Link className="inf-content" to="/inf/mat">인터넷 검색 자료</Link>
-                    <Link className="inf-content" onClick={() => alert("오픈 준비중인 기능입니다.")}>첨부 파일 자료</Link>
+                    <Link className="inf-content" to="/inf/file">첨부 파일 자료</Link>
                 </div>
             </div>
         )
