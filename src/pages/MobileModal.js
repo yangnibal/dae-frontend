ce2e749687e4024696d8cf63b46b728902082517
yangnibal <wonjun9670@naver.com>
2020-07-08 13:51:20 +0900

    no message

--- src/pages/MobileModal.js
+++ src/pages/MobileModal.js
@@ -8,18 +8,18 @@ const MobileModal = ({token, disableModal}) => {
                 <div style={{width: "30vw", display: "flex", color: "#a6a6a6", alignItems: "center", height: "6rem"}}>
                     <i onClick={disableModal} className="fas fa-times" style={{color: "#a6a6a6", fontSize: "2.5rem", marginLeft: "20px"}}></i>
                 </div>
-                <Link style={{textDecoration: "none", color: "#a6a6a6", fontSize: "1.5rem", fontWeight: "600", margin: "1.5rem 1.5rem"}} to="/inf">1급 정보</Link>
-                <Link style={{textDecoration: "none", color: "#a6a6a6", fontSize: "1.5rem", fontWeight: "600", margin: "1.5rem 1.5rem"}} to="/ac">성적 등급</Link>
-                <Link style={{textDecoration: "none", color: "#a6a6a6", fontSize: "1.5rem", fontWeight: "600", margin: "1.5rem 1.5rem"}} to="/inf/file">교재 출력</Link>
-                <a style={{textDecoration: "none", color: "#a6a6a6", fontSize: "1.5rem", fontWeight: "600", margin: "1.5rem 1.5rem"}} target="blank" href="https://blog.naver.com/dreamlearnforu">대외비 블로그</a>
-                <a style={{textDecoration: "none", color: "#a6a6a6", fontSize: "1.5rem", fontWeight: "600", margin: "1.5rem 1.5rem"}} target="blank" href="http://dreamrun.itforone.co.kr/bbs/login.php">[대외비 APP 학습 관리]</a>
-                {token===null? <Link style={{textDecoration: "none", color: "#a6a6a6", fontSize: "1.5rem", fontWeight: "600", margin: "1.5rem 1.5rem"}} to="/account/login">로그인</Link> :
+                <Link onClick={disableModal} style={{textDecoration: "none", color: "#a6a6a6", fontSize: "1.5rem", fontWeight: "600", margin: "1.5rem 1.5rem"}} to="/inf">1급 정보</Link>
+                <Link onClick={disableModal} style={{textDecoration: "none", color: "#a6a6a6", fontSize: "1.5rem", fontWeight: "600", margin: "1.5rem 1.5rem"}} to="/ac">성적 등급</Link>
+                <Link onClick={disableModal} style={{textDecoration: "none", color: "#a6a6a6", fontSize: "1.5rem", fontWeight: "600", margin: "1.5rem 1.5rem"}} to="/inf/file">교재 출력</Link>
+                <a onClick={disableModal} style={{textDecoration: "none", color: "#a6a6a6", fontSize: "1.5rem", fontWeight: "600", margin: "1.5rem 1.5rem"}} target="blank" href="https://blog.naver.com/dreamlearnforu">대외비 블로그</a>
+                <a onClick={disableModal} style={{textDecoration: "none", color: "#a6a6a6", fontSize: "1.5rem", fontWeight: "600", margin: "1.5rem 1.5rem"}} target="blank" href="http://dreamrun.itforone.co.kr/bbs/login.php">[대외비 APP 학습 관리]</a>
+                {token===null? <Link onClick={disableModal} style={{textDecoration: "none", color: "#a6a6a6", fontSize: "1.5rem", fontWeight: "600", margin: "1.5rem 1.5rem"}} to="/account/login">로그인</Link> :
                 <div style={{display: "flex", flexDirection: "column"}}>
-                    <Link style={{textDecoration: "none", color: "#a6a6a6", fontSize: "1.5rem", fontWeight: "600", margin: "1.5rem 1.5rem"}} to="/account/login">로그아웃</Link>
-                    <Link style={{textDecoration: "none", color: "#a6a6a6", fontSize: "1.5rem", fontWeight: "600", margin: "1.5rem 1.5rem"}} to="/account/edit">내정보 수정</Link>
+                    <Link onClick={disableModal} style={{textDecoration: "none", color: "#a6a6a6", fontSize: "1.5rem", fontWeight: "600", margin: "1.5rem 1.5rem"}} to="/account/login">로그아웃</Link>
+                    <Link onClick={disableModal} style={{textDecoration: "none", color: "#a6a6a6", fontSize: "1.5rem", fontWeight: "600", margin: "1.5rem 1.5rem"}} to="/account/edit">내정보 수정</Link>
                 </div> }
             </div>
-            <div style={{width: "20vw", height: "100vh", opacity: "0.6", backgroundColor: "#404040"}}></div>
+            <div onClick={disableModal} style={{width: "20vw", height: "100vh", opacity: "0.6", backgroundColor: "#404040"}}></div>
         </div>
     )
 }
