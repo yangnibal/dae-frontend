1ab10cefe0e248015792f6a78de868e269862e07
yangnibal <wonjun9670@naver.com>
2020-07-23 16:29:35 +0900

    no message

--- src/components/VidContent.js
+++ src/components/VidContent.js
@@ -6,7 +6,7 @@ const VidContent = ({name, subject, grade, group, time, watchVid}) => {
         <div className="vidcontent-container">
             <div className="vidcontent-text" onClick={watchVid}>{name}</div>
             <div className="vidcontent-text">{subject}</div>
-            <div className="vidcontent-text">{grade}</div>
+            <div className="vidcontent-text">{grade==="" ? "전체" : grade}</div>
             <div className="vidcontent-text">{group}</div>
             <div className="vidcontent-text">{time}</div>
             <div className="vidcontent-text" onClick={watchVid}>동영상 시청 하기</div>
