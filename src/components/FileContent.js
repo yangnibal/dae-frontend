63d1c9840af8b5e4d442b11629605e40e7d83363
yangnibal <wonjun9670@naver.com>
2020-08-08 23:41:16 +0900

    no message

--- src/components/FileContent.js
+++ src/components/FileContent.js
@@ -5,7 +5,7 @@ const FileContent = ({name, subject, grade, group, link, update, remove, seeFile
         <div className="filecontent-container">
             <a target="blank" href={link.charAt(link.length - 1)==="f" ? `/web/viewer.html?file=${encodeURIComponent(link)}` : link.charAt(link.length - 1)==="p" ? link : "https://docs.google.com/viewer?url=" + link} className="filecontent-text">{name}</a>
             <div className="filecontent-text">{subject}</div>
-            <div className="filecontent-text">{grade}</div>
+            <div className="filecontent-text">{grade==="" ? "전체" : grade}</div>
             <div className="filecontent-text">{group}</div>
             <a target="blank" className="filecontent-text" href={link.charAt(link.length - 1)==="f" ? `/web/viewer.html?file=${encodeURIComponent(link)}` : link.charAt(link.length - 1)==="p" ? link : "https://docs.google.com/viewer?url=" + link}>자료 보기</a>
         </div>
--- src/components/MatContent.js
+++ src/components/MatContent.js
@@ -6,7 +6,7 @@ const MatContent = ({name, subject, grade, group, link}) => {
         <div className="matcontent-container">
             <a target="blank" href={link} className="matcontent-text">{name}</a>
             <div className="matcontent-text">{subject}</div>
-            <div className="matcontent-text">{grade}</div>
+            <div className="matcontent-text">{grade==="" ? "전체" : grade}</div>
             <div className="matcontent-text">{group}</div>
             <a href={link} target="blank" className="matcontent-text">해당 자료 보러가기</a>
         </div>
