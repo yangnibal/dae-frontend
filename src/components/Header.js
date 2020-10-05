b6ee1d02def932739c8f88f7d59a237e6ef850b9
yangnibal <wonjun9670@naver.com>
2020-10-05 18:59:59 +0900

    change blog link

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,7 @@ class Header extends React.Component{
                     <Link to="/ac" className="header-data-content">성적 등급</Link>
                     <Link to="/printfile" className="header-data-content">교재 출력</Link>
                 </div>
-                <a target="blank" href="https://blog.naver.com/dreamlearnforu" className="link-dae-home">대외비 블로그<br/>바로가기</a>
+                <a target="blank" href="https://blog.naver.com/lsy4255" className="link-dae-home">대외비 블로그<br/>바로가기</a>
                 <a target="blank" href="http://dreamrun.itforone.co.kr/bbs/login.php" className="link-dae-app">[대외비 APP 학습 관리]<br/>프로그램 바로가기</a>
                 {store.getToken()===null? <Link className="link-login" to="/account/login">로그인</Link> :
                 <span className="span"><Link className="link-login" to="/account/login">로그아웃</Link>&nbsp;/&nbsp;<Link className="link-login" to="/account/edit">내정보 수정</Link></span> }
