666b1ee6952487f6f56b3d7aa9624faca0a9b8d4
yangnibal <icecube9670@gmail.com>
2021-05-24 13:30:32 +0900

    fix: remove file route target

--- src/components/MatContent.js
+++ src/components/MatContent.js
@@ -4,11 +4,11 @@ import './Content.scss'
 const MatContent = ({name, subject, grade, group, link}) => {
     return(
         <div className="matcontent-container">
-            <a target="blank" href={link} className="matcontent-text">{name}</a>
+            <a href={link} className="matcontent-text">{name}</a>
             <div className="matcontent-text">{subject}</div>
             <div className="matcontent-text">{grade==="" ? "전체" : grade}</div>
             <div className="matcontent-text">{group}</div>
-            <a href={link} target="blank" className="matcontent-text">해당 자료 보러가기</a>
+            <a href={link} className="matcontent-text">해당 자료 보러가기</a>
         </div>
     )
 }
